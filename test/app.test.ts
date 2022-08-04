db70e667ded05394ba0e28f3daed60bf1639cdf8
davidf1000 <davidfauzi29@gmail.com>
2022-08-05 06:23:16 +0700

    test: add basic test for protected and allowed method

--- test/app.test.ts
+++ test/app.test.ts
@@ -21,9 +21,107 @@ describe("GET /", () => {
   });
 });
 
-beforeEach(() => {
-  server = app.listen(process.env.PORT || 3000);
+describe("POST /register", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).post("/register");
+    expect(res.statusCode).toEqual(401);
+  });
 });
-afterEach(() => {
-  server.close();
+
+describe("POST /login", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).post("/login");
+    expect(res.statusCode).toEqual(500);
+  });
+});
+
+describe("GET /users/:userId", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).get("/users/123");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("GET /accounts", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).get("/accounts");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("POST /accounts", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).post("/accounts");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("GET /accounts/:accountId", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).get("/accounts/123");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("PUT /accounts/:accountId", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).put("/accounts/123");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("DELETE /accounts/:accountsId", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).delete("/accounts/123");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("GET /transactions", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).get("/transactions");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("POST /transactions", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).post("/transactions");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("GET /transactions/:transactionId", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).get("/transactions/123");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("PUT /transactions/:transactionId", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).put("/transactions/123");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("DELETE /transactions/:transactionId", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).delete("/transactions/123");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("GET /summary", () => {
+  it("Unauthorized", async () => {
+    const res: Response = await request(app).get("/summary");
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe("GET /randomwrongpath", () => {
+  it("Method not allowd", async () => {
+    const res: Response = await request(app).get("/randomwrongpath");
+    expect(res.statusCode).toEqual(405);
+  });
 });
